0e6279c8d34bede844ac2db7c809d412a0623b25
BrandonHebel <hebefrolfing@gmail.com>
2018-02-08 18:14:53 -0600

    cleanup

--- activitysheet/models.py
+++ activitysheet/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 from django.conf import settings
 
-class Person(models.Model):
-	name = models.CharField(max_length=30)
-
-	def __str__(self):
-		return self.name
-
 class DailyActivitySheet(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	date = models.DateField()
